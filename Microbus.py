2add8f9654b580d971bda9bf5c038ceda9eefb1d
Mahmoud Eid <mahmoud.e.abdelrauf@gmail.com>
2021-04-30 15:48:53 +0200

    Updated condition to check user input

--- Microbus.py
+++ Microbus.py
@@ -1,5 +1,13 @@
-#Version 2
-#Added condition to check user input
+#Version 3
+#Modified condition to check user input
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 
 condition = True
 
@@ -9,17 +17,16 @@ while condition:
 
     print("How much is the fare per passenger ?")
     fare = input("Enter a number: ")
-    try:
-        int(passengers)
-        print("Verified passenger input is a number")
-        int(fare)
-        print("Verified fare input is a number")
+
+    if (isfloat(passengers) & isfloat(fare)):
         condition = False
-    except:
-        print("User input is invalid")
-        
+    if not isfloat(passengers):
+        print("Passengers number was not entered correctly")
+    if not isfloat(fare):
+        print("Fare amount was not entered correctly")
+
 #the input() function returns string
 #thus we need to convert the strings to numbers as we calculate the result
-result = int(passengers) * int(fare)
+result = float(passengers) * float(fare)
 
 print("Total fare =",result)
