3a9310456653deccd610da3967f2597744f30f39
Mahmoud Eid <mahmoud.e.abdelrauf@gmail.com>
2021-04-30 12:36:56 +0200

    started microbus project
    simple input/output program with no functions or conditions

---
+++ Microbus.py
@@ -0,0 +1,11 @@
+print("How many passengers in the microbus ?")
+passengers = input("Enter a number: ")
+
+print("How much is the fare per passenger ?")
+fare = input("Enter a number: ")
+
+#the input() function returns string
+#thus we need to convert the strings to numbers as we calculate the result
+result = int(passengers) * int(fare)
+
+print("Total fare =",result)
