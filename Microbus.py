b8fe76cd669d4ae9e9f5e8f323e6cc104ac8dd25
Mahmoud Eid <mahmoud.e.abdelrauf@gmail.com>
2021-04-30 15:36:15 +0200

    Condition to check user input

--- Microbus.py
+++ Microbus.py
@@ -1,11 +1,23 @@
-#Lets start with a simple project
+#Version 2
+#Added condition to check user input
 
-print("How many passengers in the microbus ?")
-passengers = input("Enter a number: ")
+condition = True
 
-print("How much is the fare per passenger ?")
-fare = input("Enter a number: ")
+while condition:
+    print("How many passengers in the microbus ?")
+    passengers = input("Enter a number: ")
 
+    print("How much is the fare per passenger ?")
+    fare = input("Enter a number: ")
+    try:
+        int(passengers)
+        print("Verified passenger input is a number")
+        int(fare)
+        print("Verified fare input is a number")
+        condition = False
+    except:
+        print("User input is invalid")
+        
 #the input() function returns string
 #thus we need to convert the strings to numbers as we calculate the result
 result = int(passengers) * int(fare)
